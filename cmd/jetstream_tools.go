bfe1367ebdb3e2841c299ff0c15839fb104a3aa1
Oleg Shatov <oleg.shatov@second-foundation.eu>
2023-05-18 15:33:45 +0300

    Fix

--- cmd/jetstream_tools.go
+++ cmd/jetstream_tools.go
@@ -57,7 +57,7 @@ func SetConsumer(js nats.JetStreamManager, stream string, config *nats.ConsumerC
 
 func CreateOrGetObjectStoreBucket(osm nats.ObjectStoreManager, config *nats.ObjectStoreConfig) (nats.ObjectStore, error) {
 	os, err := osm.ObjectStore(config.Bucket)
-	if err != nil && !errors.Is(err, nats.ErrBucketNotFound) {
+	if err != nil && !errors.Is(err, nats.ErrBucketNotFound) && !errors.Is(err, nats.ErrStreamNotFound) {
 		return nil, err
 	}
 	if os == nil && err == nil {
